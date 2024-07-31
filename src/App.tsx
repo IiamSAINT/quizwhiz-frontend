defa99ac715ddecd61117c9c6b4793078f8bd62b
divineamunega <divineamunega@gmail.com>
2024-07-31 12:12:30 +0100

    Testing Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,29 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./features/login/Login";
 import Signup from "./features/signup/Signup";
 import Homepage from "./features/HomePage/Homepage";
+import TestingPage from "./pages/TestingPage";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/app",
-    element: <Homepage />,
-  },
+	{
+		path: "/",
+		element: <Login />,
+	},
+	{
+		path: "/signup",
+		element: <Signup />,
+	},
+	{
+		path: "/app",
+		element: <Homepage />,
+	},
+	{
+		path: "/testing",
+		element: <TestingPage />,
+	},
 ]);
 
-		element: <TestingPage />,
 function App() {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
