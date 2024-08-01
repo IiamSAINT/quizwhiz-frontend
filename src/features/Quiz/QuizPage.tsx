e8f96ddf6fc6a19789351d10c8ce9d9e096b2822
MhideTech <okefolahanolamide2006@gmail.com>
2024-08-01 11:47:04 +0100

    feat: created the create quiz page without styling

--- src/features/Quiz/QuizPage.tsx
+++ src/features/Quiz/QuizPage.tsx
@@ -3,7 +3,7 @@ import QuizSidebar from "../../ui/QuizSidebar";
 
 function QuizPage() {
   return (
-    <div className="w-11/12 mx-auto">
+    <div className="w-11/12 mx-auto flex gap-5">
       <QuizSidebar />
       <Outlet />
     </div>
