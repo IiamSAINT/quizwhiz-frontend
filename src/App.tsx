25fe2498d2cd32d67e75b3252374bb95f39409c4
divineamunega <divineamunega@gmail.com>
2025-02-03 16:27:21 +0100

    unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./features/authentication/Login";
 import Signup from "./features/authentication/Signup";
 import Homepage from "./ui/Homepage";
 import TestingPage from "./pages/TestingPage";
 import QuizPage from "./features/quiz/QuizPage";
-import CreateQuiz from "./features/quiz/CreateQuiz";
 import loginAction from "./features/authentication/LoginAction";
 import AuthProvider from "./features/authentication/AuthContext";
 import signupAction from "./features/authentication/SignupAction";
-import createQuizAction from "./features/quiz/createQuizAction";
-import AddQuestions from "./features/quiz/AddQuestions";
 import MainAppLayout from "./pages/MainAppLayout";
 const router = createBrowserRouter([
   {
