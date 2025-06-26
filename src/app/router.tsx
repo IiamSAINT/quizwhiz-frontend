95bcf83c85402865eff1596e8b0f05273dbf6646
divineamunega <divineamunega@gmail.com>
2025-06-27 00:45:10 +0100

    refactor: resturcture folder

---
+++ src/app/router.tsx
@@ -0,0 +1,53 @@
+import Landing from "@/pages/Landing";
+import SignUpPage from "@/pages/SignUpPage";
+import Login from "@/pages/Login";
+import Lobby from "@/pages/Lobby";
+import Profile from "@/pages/Profile";
+import Quiz from "@/pages/Quiz";
+import QuizFeed from "@/pages/QuizFeed";
+import NotFound from "@/pages/NotFound";
+import Layout from "@/common/components/layout/Layout";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "",
+				element: <Landing />,
+			},
+			{
+				path: "signup",
+				element: <SignUpPage />,
+			},
+			{
+				path: "login",
+				element: <Login />,
+			},
+			{
+				path: "lobby",
+				element: <Lobby />,
+			},
+			{
+				path: "profile",
+				element: <Profile />,
+			},
+			{
+				path: "quiz",
+				element: <Quiz />,
+			},
+			{
+				path: "feed",
+				element: <QuizFeed />,
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <NotFound />,
+	},
+]);
+
+export default router;
