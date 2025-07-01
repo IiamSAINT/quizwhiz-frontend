fa581c7c0d33bfb3ab73c000a7dfc6f5ac4842d8
MhideTech <okefolahanolamide2006@gmail.com>
2025-07-01 09:57:42 +0100

    feat: restructure routing to include authentication pages and add VerifyEmail component

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -7,47 +7,59 @@ import Quiz from "@/pages/Quiz";
 import QuizFeed from "@/pages/QuizFeed";
 import NotFound from "@/pages/NotFound";
 import Layout from "@/common/components/layout/Layout";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import VerifyEmail from "@/pages/VerifyEmail";
+import AuthPage from "@/pages/AuthPage";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{
-				path: "",
-				element: <Landing />,
-			},
-			{
-				path: "signup",
-				element: <SignUpPage />,
-			},
-			{
-				path: "login",
-				element: <Login />,
-			},
-			{
-				path: "lobby",
-				element: <Lobby />,
-			},
-			{
-				path: "profile",
-				element: <Profile />,
-			},
-			{
-				path: "quiz",
-				element: <Quiz />,
-			},
-			{
-				path: "feed",
-				element: <QuizFeed />,
-			},
-		],
-	},
-	{
-		path: "*",
-		element: <NotFound />,
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Landing />,
+      },
+      {
+        path: "auth",
+        element: <AuthPage />,
+        children: [
+          {
+            path: "signup",
+            element: <SignUpPage />,
+          },
+          {
+            path: "login",
+            element: <Login />,
+          },
+          {
+            path: "verify-email",
+            element: <VerifyEmail />,
+          },
+        ],
+      },
+      {
+        path: "lobby",
+        element: <Lobby />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "quiz",
+        element: <Quiz />,
+      },
+      {
+        path: "feed",
+        element: <QuizFeed />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
 
 export default router;
